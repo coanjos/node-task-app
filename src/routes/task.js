d08670d9d40d6a9218d5ebd99a281b50c812aacc
coanjos <coa.anjos@gmail.com>
2019-12-13 20:42:20 -0300

    Integration between tasks and users

--- src/routes/task.js
+++ src/routes/task.js
@@ -7,7 +7,7 @@ const router = new express.Router()
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        
+        owner: req.user._id
     })
 
     try {
